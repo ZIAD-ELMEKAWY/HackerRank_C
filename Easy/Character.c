a095289814e883e9d53e7023833d5a6002a4fd46
ZIAD_ELMEKAWY <ziadelmekawy7@gmail.com>
2023-05-05 00:09:37 +0300

    Easy_Level

---
+++ Easy/Character.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+char ch,s[50],sen[100];
+
+scanf("%c\n",&ch);
+gets(s);    
+gets(sen);
+// here we are creating an array to say that print string till find new line as input
+
+printf("%c\n",ch);
+printf("%s\n",s);
+printf("%s",sen);
+    return 0;
+}
